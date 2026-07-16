06105095c93f7c75ca03b95d904eae0dfa529751
CodeurIII <nathan2.che974@gmail.com>
2026-07-16 18:57:02 +0400

    add main

---
+++ main.ps1
@@ -0,0 +1,2 @@
+.\gradlew.bat build -x test
+cp "C:\Users\Nathan\Documents\VS Code\Create-Simurail\build\libs\simurail-1.21.1-0.0.0-a.jar" "C:\Users\Nathan\AppData\Roaming\.tlauncher\legacy\Minecraft\game\mods"
